e0bf374253f383ac715a32368a998cf8f54742da
tatiana-soifer <tatusoifer@gmail.com>
2021-08-03 23:59:39 -0300

    desafio 4

--- Desafio 4/main.js
+++ Desafio 4/main.js
@@ -1,44 +1,41 @@
-    const { Observable, fromEvent } = rxjs;
-    const { tap } = rxjs.operators;
-    const input = document.getElementById("inputText");
-    const targetScreen = document.querySelector(".target");
+const { Observable, fromEvent } = rxjs;
+const { tap } = rxjs.operators;
+const input = document.getElementById('inputText');
+const targetScreen = document.querySelector('.target');
 
-    const observable = new Observable((subscriber) => {
-      try {
-        const emitter = fromEvent(input, "keyup").pipe(
-          tap((e) => {
+const observable = new Observable((suscriptor) => {
+    try {
+        const emitter = fromEvent(input, 'keyup').pipe(
+            tap((e) => {
             const targetValue = e.target.value;
-            if (targetValue === "error") subscriber.error("Error ingresado");
-            if (targetValue === "complete") subscriber.complete();
-          })
+            if (targetValue === 'error') suscriptor.error('Error ingresado');
+            if (targetValue === 'complete') suscriptor.complete();
+            })
         );
-        emitter.subscribe(subscriber);
-
+        emitter.subscribe(suscriptor);
         setTimeout(() => {
-          subscriber.complete();
+            suscriptor.complete();
         }, 30000);
-      } catch (error) {
-        subscriber.error(error);
-      }
-    });
-
-    observable.subscribe({
-      next(e) {
-        const targetArr = e.target.value.split("");
-        targetScreen.textContent = targetArr.reverse().join("");
-      },
-      complete() {
-        stopProcesses();
-        console.log("completado!");
-      },
-      error(error) {
-        console.error(error);
-      },
-    });
-
-    const stopProcesses = () => {
-      input.value = "";
-      input.disabled = true;
-      targetScreen.textContent = "";
+    } catch (error) {
+        suscriptor.error(error);
     };
+});
+observable.subscribe({
+    next(e) {
+    const targetArr = e.target.value.split('');
+        targetScreen.textContent = targetArr.reverse().join('');
+    },
+    complete() {
+        stop();
+        console.log("completado!");
+    },
+    error(error) {
+        console.error(error);
+    },
+});
+const stop = () => {
+    input.value = '';
+    input.disabled = true;
+    targetScreen.textContent = '';
+};
 
